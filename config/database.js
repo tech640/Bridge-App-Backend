0799cfac6bc5215d97a3a4dd5dbd81751f9dae5a
hadeelMatar <tech@codex-college.com>
2026-04-06 11:24:10 +0300

    commit2

--- config/database.js
+++ config/database.js
@@ -37,10 +37,10 @@ const initializeDatabase = async() =>{
         await client.query(`
         CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
+        name VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        role VARCHAR(20) DEFAULT 'user',
+        phone VARCHAR(20) NOT NULL,
+        role_id INT REFERENCES roles(id),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
